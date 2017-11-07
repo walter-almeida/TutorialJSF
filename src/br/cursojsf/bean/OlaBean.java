901b538666625d2cada7e625ad76c0815f207fe1
Walter Teixeira de Almeida - DATAPREVRJ <walter.almeida@RJOD212010.GOV>
2017-11-07 11:06:15 -0200

    commit inicial

---
+++ src/br/cursojsf/bean/OlaBean.java
@@ -0,0 +1,22 @@
+package br.cursojsf.bean;
+
+import java.io.Serializable;
+
+public class OlaBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
