3a40fff776f1040c82196f7decd00ac7326fffb2
walter.almeida <walter.almeida@RJOD212010.GOV>
2017-11-08 12:21:03 -0200

    Converter Internacionalização

---
+++ src/br/com/cursojsf/bean/CadastroBean.java
@@ -0,0 +1,62 @@
+package br.com.cursojsf.bean;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class CadastroBean implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String nome;
+	private String email;
+	private String bairro;
+	private Date dataNascimento;
+	private ArrayList bairros;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public ArrayList getBairros() {
+		return bairros;
+	}
+
+	public void setBairros(ArrayList bairros) {
+		this.bairros = bairros;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	
+	
+	
+}
---
+++ src/br/com/cursojsf/control/Controle.java
@@ -0,0 +1,16 @@
+package br.com.cursojsf.control;
+
+import java.io.Serializable;
+
+public class Controle implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public void acao() {
+		
+	}
+
+}
